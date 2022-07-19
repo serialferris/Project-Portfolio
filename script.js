6a48489d0b166c76c9f3d9d8f792494e99e494ed
serialferris <57386627+serialferris@users.noreply.github.com>
2022-07-19 20:19:39 +0530

    updated script.js

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ hamburger.addEventListener('click', () => {
     navMenu.classList.add('active');
     closeIcon.classList.add('show');
 });
+
+closeIcon.addEventListener('click', () => {
+    closeIcon.classList.remove('show');
+    navMenu.classList.remove('active');
+    hamburger.classList.add('bars');
+  });
+  
+  links.forEach((n) => n.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+    closeIcon.classList.remove('show');
+    hamburger.classList.add('bars');
+  }));
+  
